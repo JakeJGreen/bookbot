f8a9299fb4f7aa5a30ed2682a6648ed5510128b4
jakejgreen <jakejjgreen@gmail.com>
2025-06-30 12:15:22 +0100

    D: Add two new books and accept second arguement

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 import os
+import sys
+
 from stats import get_num_words,get_num_char,sort_dict
-def main(): 
-    path = "books/frankenstein.txt"
-    content = get_book_text(path)
-    #print("============ BOOKBOT ============")
-    #print(f"Analyzing book found at {path}...")
-    #print("----------- Word Count ----------")
-    get_num_words(content)
-    #print("----------- Character Count ----------")
-    #get_num_char(content)
-    num_of_chars = get_num_char(content)
-    #print(num_of_chars)
-    report = sort_dict(num_of_chars)
-    for k,v in report.items():
-        print(f"{k}: {v}")
-    #print (content)
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        content = get_book_text(sys.argv[1])
+        #print("============ BOOKBOT ============")
+        #print(f"Analyzing book found at {path}...")
+        #print("----------- Word Count ----------")
+        get_num_words(content)
+        #print("----------- Character Count ----------")
+        #get_num_char(content)
+        num_of_chars = get_num_char(content)
+        #print(num_of_chars)SD
+        report = sort_dict(num_of_chars)
+        for k,v in report.items():
+            print(f"{k}: {v}")
     
+        
 def get_book_text(file):
     if os.path.exists(file):
         with open(file) as book:
